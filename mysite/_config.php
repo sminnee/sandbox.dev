7f19925f48c6d929a5db5e649ebe35b9627bfd3d
Stig Lindqvist <stojg.lindqvist@gmail.com>
2014-06-08 22:04:02 +1200

    Upgrading to silverstirpe 3.1.5 and setting default theme

--- mysite/_config.php
+++ mysite/_config.php
@@ -10,7 +10,7 @@ require_once('conf/ConfigureFromEnv.php');
 
 // Set the current theme. More themes can be downloaded from
 // http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
+SSViewer::set_theme('pure');
 
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
