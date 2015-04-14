67a3362d15e81163a598cec7dfc51862be4e85fb
Stig Lindqvist <stojg.lindqvist@gmail.com>
2015-04-14 12:23:13 +1200

    Add environmnet checks

--- mysite/_config.php
+++ mysite/_config.php
@@ -14,3 +14,9 @@ SSViewer::set_theme('pure');
 
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if(class_exists('SiteTree')) SiteTree::enable_nested_urls();
+
+// public
+EnvironmentCheckSuite::register('pingdom', 'URLCheck("")', "Homepage accessible");
+EnvironmentCheckSuite::register('pingdom', 'DatabaseCheck', "Connect to database");
+EnvironmentCheckSuite::register('pingdom', 'ExternalURLCheck("https://stojg.se/", "5")', "Connect to stojg.se?");
+
