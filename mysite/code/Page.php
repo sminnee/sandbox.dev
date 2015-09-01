3bb9f1831a2f44ebecc04976b66c8d31f19fe182
Sean Harvey <sean@silverstripe.com>
2015-09-01 14:45:30 +1200

    Don't show dashes without version, ensure specific versions override.

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -8,29 +8,28 @@ class Page extends SiteTree {
 		natcasesort($extensions);
 		foreach($extensions as $extension) {
 			$version = phpversion($extension);
-			if(!$version && $extension == 'gd') {
+			if($extension == 'gd') {
 				$info = function_exists('gd_info') ? gd_info() : array();
 				$version = isset($info['GD Version']) ? $info['GD Version'] : '';
 			}
-			if(!$version && $extension == 'pcre') {
+			if($extension == 'pcre') {
 				$version = PCRE_VERSION;
 			}
-			if(!$version && $extension == 'curl') {
+			if($extension == 'curl') {
 				$version = curl_version()['version'];
 			}
-			if(!$version && $extension == 'openssl') {
+			if($extension == 'openssl') {
 				$version = OPENSSL_VERSION_TEXT;
 			}
-			if(!$version && $extension == 'libxml') {
+			if($extension == 'libxml') {
 				$version = LIBXML_DOTTED_VERSION;
 			}
-
 			// Extension version shows as 0.7-dev, which isn't useful. Use SQLite3::version() instead.
 			if($extension == 'sqlite3') {
 				$version = SQLite3::version()['versionString'];
 			}
 			$phpExtensionList->push(new ArrayData([
-				'Value' => sprintf('%s - %s', $extension, $version)
+				'Value' => $version ? sprintf('%s - %s', $extension, $version) : $extension
 			]));
 		}
 
